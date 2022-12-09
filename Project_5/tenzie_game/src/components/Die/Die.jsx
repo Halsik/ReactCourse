2c05828b5d140c6ab8f74860335d82740a7a618b
Halsik <michal.jan.hals@gmail.com>
2022-12-09 12:29:10 +0100

    project 5 finished / Course completed

--- Project_5/tenzie_game/src/components/Die/Die.jsx
+++ Project_5/tenzie_game/src/components/Die/Die.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import './Die.css'
 
-function Die() {
+function Die(props) {
+
+    const styles = {
+        backgroundColor: props.toggle ? "#59E391" : "white"
+    }
+   
     return(
-        <div className="die">
-            <p className="die-number">1</p>
+        <div  onClick={() => props.showDice(props.id)} style={styles}className="die">
+            <p className="die-number">{props.value}</p>
         </div>
     )
 }
--- Project_5/tenzie_game/src/components/Hero/Hero.jsx
+++ Project_5/tenzie_game/src/components/Hero/Hero.jsx
@@ -1,22 +1,94 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './Hero.css'
 import Die from "../Die/Die";
+import Confetti from 'react-confetti'
 
 function Hero() {
+
+    const getNewDice = () => {
+        return {
+            number:Math.ceil(Math.random() * 6),
+            isHold:false,
+            id: Math.ceil(Math.random() * 2000)
+        }
+    }
+   
+
+    const allNewDice = () => {
+
+        const arrayOfNumbers = []
+        
+        
+        
+        for(let i = 0; i < 10; i ++) {
+
+            arrayOfNumbers.push(getNewDice())
+        }
+
+
+        
+
+        return arrayOfNumbers
+    }
+    
+
+    const hold = (id) => {
+        setDiceNumber(oldDiceNumber => oldDiceNumber.map(die => {
+            return die.id === id ? {...die, isHold: !die.isHold} : die
+        }))
+    }
+
+    const [diceNumbers, setDiceNumber] = useState(allNewDice)
+
+    const [tenzie, setTenzie] = useState(false)
+
+   useEffect(() => {
+     const isAllHeld  = diceNumbers.every(die => die.isHold)
+     const firstValue = diceNumbers[0].number
+     const allSameValues = diceNumbers.every(die => die.number === firstValue)
+
+     if(isAllHeld && allSameValues) {
+        setTenzie(true)
+        console.log("wygrales")
+     }
+   }, [diceNumbers])
+
+    const newDiceArray = diceNumbers.map(dice => {
+        return <Die value={dice.number}
+                    toggle={dice.isHold}
+                    key={dice.id}
+                    showDice={hold}
+                    id={dice.id}/>
+    })
+
+    function newDice() {
+        if(tenzie === false) {
+        setDiceNumber(oldDice => oldDice.map(dice => {
+            return dice.isHold ? dice : getNewDice()
+        }))} else if(tenzie) {
+            setDiceNumber(allNewDice)
+            setTenzie(false)
+        }
+    }
+
+    const width = `100%`
+
+    
+
+
+    
+
+
     return(
         <div className="hero-container">
+            { tenzie && <Confetti 
+            width={1440}
+            height={750}/>}
             <div className="hero-die-container">
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
-                <Die />
+               {newDiceArray}
+                
             </div>
+            <button onClick={newDice}>{tenzie ? "Play Again" : "Roll Dice"}</button>
         </div>
     )
 }
